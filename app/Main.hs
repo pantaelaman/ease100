2d83b920fcce3b402ec06adce9ed334fac0c5384
pantaelaman <calvin@theophoffs.com>
2025-02-22 16:07:36 -0500

    cross-compilation added

--- app/Main.hs
+++ app/Main.hs
@@ -17,7 +17,11 @@ import Control.Monad.Cont
 
 import Parser
 
-data Args = Args { fname :: String, mifname :: Maybe String, lblname :: Maybe String }
+data Args = Args
+  { fname :: String
+  , mifname :: Maybe String
+  , lblname :: Maybe String
+  , crossname :: Maybe String }
 
 argParser :: O.Parser Args
 argParser = Args
@@ -30,11 +34,15 @@ argParser = Args
     (  O.metavar "LABELS FILE"
     <> O.short 'l'
     <> O.long "labels"))
+  <*> optional (O.strOption
+    (  O.metavar "E FILE"
+    <> O.short 'e'
+    <> O.long "cross"))
 
 argInfo :: O.ParserInfo Args
 argInfo = O.info (argParser <**> O.helper)
   ( O.fullDesc
-  <> O.progDesc "Compiles FILE, generating a .mif and .labels file"
+  <> O.progDesc "Compiles FILE, generating a .mif and .labels file; also optionally a .e file"
   <> O.header "ease - a compiler for extended ase100 programs")
 
 main :: IO ()
@@ -56,12 +64,20 @@ main = do
         hPutStr mifFile "DEPTH = 16384;\r\nWIDTH = 32;\r\nADDRESS_RADIX = DEC;\r\nDATA_RADIX = DEC;\r\nCONTENT\r\nBEGIN\r\n"
         mapM_ (hPutStr mifFile) $ map (uncurry wordToMIF) $ zip [0..] bytecode
         hPutStr mifFile "END;\r\n \r\n\r\n"
-
         mapM_ (hPutStrLn lblFile . uncurry wordToLBL) $ sortBy lblOrdering $ HM.toList $ st^.pstLbls
+
+      case crossname args of
+        Just crossn -> do
+          crossFile <- ContT $ withFile crossn WriteMode
+          liftIO $ mapM_ (hPutStr crossFile . wordToE) $ bytecode
+        Nothing -> return ()
+
   where
     wordToMIF :: Int -> Word32 -> String
     wordToMIF i v = "\t" ++ show i ++ "\t:\t" ++ show (unsafeCoerce v :: Int32) ++ ";\r\n"
     wordToLBL :: String -> Word32 -> String
     wordToLBL lbl val = lbl ++ "\t" ++ show val ++ "\t" ++ "(0x" ++ showHex val ")"
+    wordToE :: Word32 -> String
+    wordToE v = "  " ++ show v ++ "\n"
     lblOrdering :: (String, Word32) -> (String, Word32) -> Ordering
     lblOrdering (_, a) (_, b) = compare a b
