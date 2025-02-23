3d9236669a2321e582515edfc810d3a5cc4ea6ca
pantaelaman <calvin@theophoffs.com>
2025-02-23 12:20:27 -0500

    started adding comments

--- app/Parser.hs
+++ app/Parser.hs
@@ -28,40 +28,77 @@ instance Show Value where
   show (VLabel lbl) = "VLabel " ++ show lbl
   show (VConstExpr v1 v2 _) = "VConstExpr (" ++ show v1 ++ " ? " ++ show v2 ++ ")"
 
+-- This actually holds both labels and define directives in one place:
+-- Left holds labels (with literal addresses)
+-- Right holds values (from define directives)
+-- Labels can't be converted to values because trying to parse a value out of context
+-- (like when generating a .labels file)
+-- could possibly generate parse errors, so they have to be stored as literals.
+-- Small side effect: define directives that aren't used anywhere can actually have
+-- invalid labels in their definitions! Since the value never gets parsed, it'll never be discovered.
+-- Oops! Should probably fix that at some point, but it's not a breaking bug.
 type LabelMap = HM.HashMap String (Either Word32 Value)
+
+-- This holds all the information needed to tie parse errors happening in included files
+-- to the proper file. Due to how megaparsec handles errors, ParseErrorBundles are made up of
+-- several errors with integer offsets and a single source info which gives the source in its
+-- entirety. This means that any individual error doesn't have any record of where its from,
+-- so we have to keep track of that manually.
 type SrcMap = HM.HashMap String SrcInfo
+-- ParseErrorBundle parameterised by our constraints
 type InternalPEB = ParseErrorBundle T.Text Void
 
+-- Inclusion offset is here so that we can generate a reasonable error in the file in which
+-- they were included. The PosState is what the ParseErrorBundle needs for context.
 data SrcInfo = SrcInfo {
   srcInclusionOffset :: Int,
   srcPosState :: PosState T.Text }
   deriving Show
 
 data PState = PState {
-  _pstAddr :: Word32,
-  _pstLbls :: LabelMap,
-  _pstErrs :: [InternalPEB],
-  _pstSrcs :: SrcMap }
+  _pstAddr :: Word32, -- current address, monotonically increases across parsing
+  _pstLbls :: LabelMap, -- holds labels and define directives
+  _pstErrs :: [InternalPEB], -- holds remote file ParseErrorBundles
+  _pstSrcs :: SrcMap } -- contains information on sources for linking with PEBs
   deriving Show
 makeLenses ''PState
 
+-- Why this monad stack?
+-- 1. IO is necessary for including files.
+-- 2. StateT is necessary for holding the parser's context; it has to go outside of
+--    ParsecT because it holds all ParseErrorBundles for remote files. Due to how ParsecT
+--    works, it can only be considered to parsing one source file at a time, so other file's
+--    errors have to be stored elsewhere (in PState). An error in an included file generates
+--    an error in the main file which would invalidate PState if it were within ParsecT.
+-- 3. ParsecT is necessary for obvious reasons.
 type Parser = ParsecT Void T.Text (StateT PState IO)
 
+-- Convenience
 newPState :: PState
 newPState = PState 0 HM.empty [] HM.empty
 
+-- Instr opcode arg1 arg2 arg3
 data Instr = Instr Word32 Value Value Value
   deriving Show
+
+-- Chunks are units which represent the smallest units of the language.
+-- Padding is included here for efficiency.
 data Chunk = CInstr Instr | Lit Value | Padding Word32
   deriving Show
 
+-- Keeps track of where a chunk came from in the source for proper error handling
+-- during the second pass. Local offsets come from the actively parsed file. At the top
+-- level, this is the source file being parsed. Remote offsets are generated when including
+-- a file; when parsing these all local offsets are converted to remote offsets.
 data ChunkOffset = COLocal Int | CORemote Int String
   deriving Show
 
+-- Converts the local offsets to remote offsets by applying a specific source.
 applyRemote :: String -> ChunkOffset -> ChunkOffset
 applyRemote _ c@(CORemote _ _) = c -- remote files don't get more remote
 applyRemote src (COLocal offset) = CORemote offset src
 
+-- The values here are just dummy data; this is basically a hash set of reserved keywords.
 instrOpcodes :: SH.StaticHash String Word32
 instrOpcodes = SH.fromList [
   ("halt", 0),
@@ -84,19 +121,24 @@ instrOpcodes = SH.fromList [
   ("call", 16),
   ("ret", 17)]
 
+-- Useful shorthand for parseInstr
 vzero :: Value
 vzero = VInt 0
 
+-- Calculate a chunk's size; every chunk must have a trivial size computable on the first pass
+-- so that labels can be solved immediately.
 size :: Chunk -> Word32
 size (CInstr _) = 4
 size (Lit _) = 1
 size (Padding paddingSize) = paddingSize
 
+-- Top level parsing, called from main
 parse :: String -> IO ((Either InternalPEB [Word32], PState))
 parse fname = do
   ftext <- TIO.readFile fname
   runStateT (runParserT parser fname ftext) newPState
 
+-- Root parser, compiles chunks into words
 parser :: Parser [Word32]
 parser = do
   chunks <- chunker
@@ -108,14 +150,17 @@ parser = do
     evalChunk lbls offset (Lit val) = singleton <$> evalValue lbls offset val
     evalChunk lbls offset (CInstr (Instr opcode v1 v2 v3)) = sequence $
       return opcode : evalValue lbls offset v1 : evalValue lbls offset v2 : evalValue lbls offset v3 : []
+    -- evaluates values by pulling values from the LabelMap
     evalValue :: LabelMap -> ChunkOffset -> Value -> Parser Word32
     evalValue _ _ (VInt n) = return n
     evalValue lbls offset (VLabel lbl) = case HM.lookup lbl lbls of
-      Just (Right val) -> evalValue lbls offset val
-      Just (Left addr) -> return addr
-      Nothing -> errAtOffset lbl offset
+      Just (Right val) -> evalValue lbls offset val -- eval define directive values
+      Just (Left addr) -> return addr -- simple label address
+      Nothing -> errAtOffset lbl offset -- missing label
     evalValue lbls offset (VConstExpr v1 v2 opr) =
       opr <$> evalValue lbls offset v1 <*> evalValue lbls offset v2
+    -- generates a label not found error at the appropriate offset for a specific chunk
+    -- this includes handling generating all necessary errors in the case of a remote file
     errAtOffset :: String -> ChunkOffset -> Parser a
     errAtOffset lbl (COLocal offset) = region (setErrorOffset offset) $
       fail $ "label \"" ++ lbl ++ "\" doesn't exist"
@@ -127,31 +172,37 @@ parser = do
       region (setErrorOffset $ srcInclusionOffset srcInfo) $
         fail $ "error in included file \"" ++ src ++ "\""
 
+-- end of line or end of input; convenience
 eol :: Parser ()
 eol = eof <|> (C.eol >> return ())
 
+-- generates chunks from sources; the real meat of the parsing
 chunker :: Parser [(ChunkOffset, Chunk)]
 chunker = concat <$> manyTill
   (try parseDirective <|> try parseLine <|> (whitespace >> eol >> return [])) eof
   where
     parseLine :: Parser [(ChunkOffset, Chunk)]
     parseLine = do
-      lblOffset <- getOffset
+      -- attempts to parse a label
+      lblOffset <- getOffset -- save the offset for cleaner error reporting
       (optional $ try $ L.nonIndented whitespace safeIdentifier) >>= \x ->
         case x of
           Just lbl -> do
             st <- get
             if HM.member lbl (st^.pstLbls) then
+              -- errs in the event a label has already been defined
               region (setErrorOffset lblOffset) $ fail $ "duplicate label " ++ lbl
             else pstLbls %= HM.insert lbl (Left $ st^.pstAddr)
           Nothing -> return ()
 
       whitespace
 
+      -- parses and applies offset appropriately to instruction chunks
       offset <- getOffset
       chunks <- try (singleton . ((,) $ COLocal offset) . CInstr <$> parseInstr) <|> try parseNonInstr
       -- increment addr based on chunk sizes
       mapM (\c -> ((pstAddr %= (+) (size $ snd c)) *> return c)) chunks <* eol
+    -- collect many values into multiple chunks
     parseNonInstr :: Parser [(ChunkOffset, Chunk)]
     parseNonInstr = do
       fmap concat $ many $ do
@@ -161,10 +212,12 @@ chunker = concat <$> manyTill
           <|> try parseString
           <|> try (singleton . Padding <$> parsePadding)
 
+-- anything following a `#`
 parseDirective :: Parser [(ChunkOffset, Chunk)]
 parseDirective = label "directive" $
   C.char '#' >> (try parseIncludeDirective <|> try parseDefineDirective)
 
+-- #include
 parseIncludeDirective :: Parser [(ChunkOffset, Chunk)]
 parseIncludeDirective = label "include directive" $ do
   _ <- L.symbol whitespace (T.pack "include")
@@ -177,17 +230,22 @@ parseIncludeDirective = label "include directive" $ do
     (parseResult, newst) <- liftIO $ runStateT (runParserT chunker fname ftext) st
     put newst
     case parseResult of
-      Left peb ->
+      -- if there's a parse error, generate one in this file at the point of inclusion
+      Left peb -> 
         (pstErrs %= (:) peb)
           >> region (setErrorOffset offset) (fail $ "error in included file \"" ++ fname ++ "\"")
+      -- turn all local chunk offsets in this file into remote offsets
       Right chunks -> return $ map (first $ applyRemote fname) $ chunks
   where
+    -- validation? what's that
     filename :: Parser String
     filename = manyTill anySingle eol
 
+-- #define
 parseDefineDirective :: Parser [(ChunkOffset, Chunk)]
 parseDefineDirective = label "define directive" $ do
   _ <- L.symbol whitespace (T.pack "define")
+  -- save offset for better error reporting
   lblOffset <- getOffset
   lbl <- lexeme safeIdentifier
   val <- parseValue
@@ -195,7 +253,10 @@ parseDefineDirective = label "define directive" $ do
   if HM.member lbl $ st^.pstLbls then
     region (setErrorOffset lblOffset) $ fail $ "duplicate label \"" ++ lbl ++ "\""
   else
+  -- create new "label" - see LabelMap
     pstLbls %= HM.insert lbl (Right val)
+
+  -- doesn't actually do anything to the source
   return []
 
 parsePadding :: Parser Word32
@@ -218,10 +279,11 @@ parseInstr = lexeme (label "instruction" identifier) >>= \raw_instr -> do
     "mult" -> ternary 3
     "div" -> ternary 4
     "cp" -> binary 5
-    "cpdata" -> do
+    "cpdata" -> do -- complicated because we have to do some wacky stuff with addresses
       target <- parseValue
       val <- parseValue
       st <- get
+      -- we can do this because when parsing instructions, addr will always be up to date
       return $ Instr 5 target (VInt $ st^.pstAddr + 3) val
     "and" -> ternary 6
     "or" -> ternary 7
